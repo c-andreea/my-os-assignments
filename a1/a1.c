525652bbac843fcccdfbdcc6323d4a7cc128c3f7
c-andreea <92252346+c-andreea@users.noreply.github.com>
2023-04-04 11:59:21 +0300

    1

---
+++ a1/a1.c
@@ -0,0 +1,14 @@
+//
+// Created by andreea on 03.04.2023.
+//
+
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, char **argv){
+    if(argc>=2)
+        if(strstr(argv[1],"variant"))
+            printf("885285\n");
+    return 0;
+
+}
