0ded363d7f17b8d1e0e99c3a8cfded0de05769c3
c-andreea <92252346+c-andreea@users.noreply.github.com>
2023-04-08 17:17:41 +0300

    teste pana la greater_6

--- a1/a1.c
+++ a1/a1.c
@@ -5,6 +5,8 @@
 #include <dirent.h>
 #include <sys/stat.h>
 #include <stdbool.h>
+#include <fcntl.h>
+#include <unistd.h>
 
 typedef struct {
     char magic;
@@ -26,50 +28,6 @@ typedef struct {
     SectionHeader *section_headers;
 } SFFile;
 
-bool parse_sf_file(const char *file_path) {
-    FILE *file = fopen(file_path, "rb");
-    if (!file) {
-        perror("Error opening file");
-        return false;
-    }
-
-    SFHeader sf_header;
-    if (fread(&sf_header, sizeof(SFHeader), 1, file) != 1) {
-        fclose(file);
-        return false;
-    }
-
-    if (sf_header.magic != 'Y' ||
-        sf_header.version < 14 || sf_header.version > 124 ||
-        sf_header.no_of_sections < 4 || sf_header.no_of_sections > 20) {
-        fclose(file);
-        return false;
-    }
-
-    SectionHeader *section_headers = (SectionHeader *)malloc(sf_header.no_of_sections * sizeof(SectionHeader));
-    if (!section_headers) {
-        fclose(file);
-        return false;
-    }
-
-    if (fread(section_headers, sizeof(SectionHeader), sf_header.no_of_sections, file) != sf_header.no_of_sections) {
-        free(section_headers);
-        fclose(file);
-        return false;
-    }
-
-    for (int i = 0; i < sf_header.no_of_sections; i++) {
-        if (section_headers[i].sect_type != 45 && section_headers[i].sect_type != 16) {
-            free(section_headers);
-            fclose(file);
-            return false;
-        }
-    }
-
-    free(section_headers);
-    fclose(file);
-    return true;
-}
 bool ends_with(const char *str, const char *suffix) {
     if (!str || !suffix) {
         return false;
@@ -79,12 +37,14 @@ bool ends_with(const char *str, const char *suffix) {
     if (suffix_len > str_len) {
         return false;
     }
-    return strncmp(str + str_len - suffix_len, suffix, suffix_len) == 0;
+    return strcasecmp(str + str_len - suffix_len, suffix) == 0;
 }
 
+bool size_greater_than(off_t size, off_t threshold) {
+    return size > threshold;
+}
 
-
-void list_directory(const char *dir_path, int recursive, const char *name_ends_with, off_t size_greater) {
+void list_directory(const char *dir_path, int recursive, const char *name_ends_with, off_t size_greater, const char *size_greater_suffix) {
     DIR *dir = opendir(dir_path);
     if (!dir) {
         perror("Error opening directory");
@@ -95,26 +55,45 @@ void list_directory(const char *dir_path, int recursive, const char *name_ends_w
     struct stat st;
     char full_path[1024];
     while ((entry = readdir(dir)) != NULL) {
-        if (strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0) {
-            continue;
-        }
+        if (strcmp(entry->d_name, ".") != 0 && strcmp(entry->d_name, "..") != 0) {
+            snprintf(full_path, sizeof(full_path), "%s/%s", dir_path, entry->d_name);
 
-        snprintf(full_path, sizeof(full_path), "%s/%s", dir_path, entry->d_name);
+            if (lstat(full_path, &st) == 0) {
+                if (S_ISDIR(st.st_mode)) {
 
-        if (lstat(full_path, &st) == 0) {
-            if (S_ISDIR(st.st_mode)) {
-                if (name_ends_with && ends_with(entry->d_name, name_ends_with)) {
+
+                    if (recursive) {
+                        list_directory(full_path, recursive, name_ends_with, size_greater, size_greater_suffix);
+                    }
+
+                    if(size_greater==-1){
+
+
+                        if (name_ends_with != NULL && !ends_with(full_path, name_ends_with)) {
+                            continue;
+                        }
+
+                        if (size_greater_suffix && !ends_with(full_path, size_greater_suffix)) {
+                            continue;
+                        }
+                        
+                        printf("%s\n",full_path);
+                    }
+                } else if (S_ISREG(st.st_mode)) {
+                    if (name_ends_with != NULL && !ends_with(full_path, name_ends_with)) {
+                        continue;
+                    }
+
+                    if (size_greater_suffix && !ends_with(full_path, size_greater_suffix)) {
+                        continue;
+                    }
+
+                    if (size_greater != -1 && !size_greater_than(st.st_size, size_greater)) {
+                        continue;
+                    }
+
                     printf("%s\n", full_path);
                 }
-
-                if (recursive) {
-                    list_directory(full_path, recursive, name_ends_with, size_greater);
-                }
-            } else if (S_ISREG(st.st_mode)) {
-                if ((size_greater == -1 || st.st_size > size_greater) && (!name_ends_with || ends_with(entry->d_name, name_ends_with))) {
-                    printf("%s\n", full_path);
-                }
-
             }
         }
     }
@@ -122,50 +101,36 @@ void list_directory(const char *dir_path, int recursive, const char *name_ends_w
     closedir(dir);
 }
 
+
 int main(int argc, char **argv) {
     if (argc >= 2) {
         if (strcmp(argv[1], "variant") == 0) {
             printf("88528\n");
         } else if (strcmp(argv[1], "parse") == 0) {
-            const char *file_path = NULL;
+
             for (int i = 1; i < argc; i++) {
                 if (strncmp(argv[i], "path=", 5) == 0) {
-                    file_path = argv[i] + 5;
+
                 }
             }
-
-            if (!file_path) {
-                fprintf(stderr, "ERROR\nmissing file path\n");
-                return 1;
-            }
-
-            if (parse_sf_file(file_path)) {
-                printf("SUCCESS\n");
-                // You can print additional information about the SF file here, if required.
-            } else {
-                printf("ERROR\nwrong magic|version|sect_nr|sect_types\n");
-            }
-        } else if (strcmp(argv[1], "list") == 0) {
-            // The existing code for the "list" command goes here.
-        }
-    }
-
-
-
+        }}
     int recursive = 0;
     const char *dir_path = NULL;
     const char *name_ends_with = NULL;
     off_t size_greater = -1;
+    const char *size_greater_suffix = NULL;
 
     for (int i = 1; i < argc; i++) {
         if (strcmp(argv[i], "recursive") == 0) {
             recursive = 1;
         } else if (strncmp(argv[i], "path=", 5) == 0) {
             dir_path = argv[i] + 5;
-        } else if (strncmp(argv[i], "name ends with=", 15) == 0) {
-            name_ends_with = argv[i] + 15;
-        } else if (strncmp(argv[i], "size greater=", 13) == 0) {
+        } else if (strncmp(argv[i], "name_ends_with=", 15) == 0) {
+            name_ends_with= argv[i] + 15;
+        } else if (strncmp(argv[i], "size_greater=", 13) == 0) {
             size_greater = strtoll(argv[i] + 13, NULL, 10);
+        } else if (strncmp(argv[i], "size_greater_", 14) == 0) {
+            size_greater_suffix = argv[i] + 14;
         }
     }
 
@@ -176,7 +141,7 @@ int main(int argc, char **argv) {
 
 
     printf("SUCCESS\n");
-    list_directory(dir_path, recursive, name_ends_with, size_greater);
+    list_directory(dir_path, recursive, name_ends_with, size_greater, size_greater_suffix);
 
     return 0;
 }
